cd57ccbf48128a5a19d983f7dd4addb753ef2c9d
SomRav <soumyachakra004@gmail.com>
2025-03-22 03:00:22 +0530

    part1 complete exersise 1.12-1.14 (anecdotes)

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,6 +1,32 @@
 import { useState } from "react";
 import React from "react";
 
+const Header = ({ text }) => {
+  return <h2>{text}:</h2>;
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Display = ({ anecdote, vote }) => {
+  return (
+    <p>
+      {anecdote} <br /> has {vote} votes
+    </p>
+  );
+};
+const Mostvote = ({ anecdotes, votes }) => {
+  const maxVote = Math.max(...votes);
+  if (maxVote === 0) {
+    return <p>No votes given</p>;
+  }
+  const indexOfMax = votes.indexOf(maxVote);
+  const anecdote = anecdotes[indexOfMax];
+
+  return <Display anecdote={anecdote} vote={maxVote} />;
+};
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -12,10 +38,34 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
+  const totalAnecdotes = anecdotes.length;
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(totalAnecdotes).fill(0));
 
-  return <div>{anecdotes[selected]}</div>;
+  const randomIndex = (max = totalAnecdotes) => Math.floor(Math.random() * max);
+
+  const handleNextAnecdote = () => {
+    const index = randomIndex();
+    setSelected(index);
+  };
+  const handleVote = () => {
+    const selectedIndex = selected;
+    const copyVotes = [...votes];
+    copyVotes[selectedIndex] += 1;
+    setVotes(copyVotes);
+  };
+
+  return (
+    <div>
+      <Header text={"Anecdote of the day"} />
+      <Display anecdote={anecdotes[selected]} vote={votes[selected]} />
+      <Button onClick={handleVote} text={"Vote"} />
+      <Button onClick={handleNextAnecdote} text={"Next anecdote"} />
+      <Header text={"Anecdote with most votes"} />
+      <Mostvote anecdotes={anecdotes} votes={votes} />
+    </div>
+  );
 };
 
 export default App;
